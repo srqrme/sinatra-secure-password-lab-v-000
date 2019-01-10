38fc92b4a762bca90a7e09666ba3caf0c9bf25e7
Sharmaine Requerme <srqrme@gmail.com>
2019-01-10 20:38:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ class ApplicationController < Sinatra::Base
     ##your code here
     @user = User.find_by(:username => params[:username])
 
-    if user && user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect "/account"
     else
